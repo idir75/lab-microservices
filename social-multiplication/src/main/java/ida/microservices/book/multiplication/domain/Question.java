218fb185eabf554679761ac9ae4794ed7d54a1da
idah <idir.dahmouh@gmail.com>
2018-03-18 18:22:35 +0100

    Add Question entity

---
+++ social-multiplication/src/main/java/ida/microservices/book/multiplication/domain/Question.java
@@ -0,0 +1,25 @@
+package ida.microservices.book.multiplication.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "QUESTION_ID")
+    private Long id;
+
+}
