e6402aedbdf0f1a9789387cf834ce56a9f259856
idah <idir.dahmouh@gmail.com>
2018-03-18 18:32:35 +0100

    Add qcm project

---
+++ qcm/src/main/java/ida/microservices/qcm/QcmApplication.java
@@ -0,0 +1,12 @@
+package ida.microservices.qcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcmApplication.class, args);
+	}
+}
---
+++ qcm/src/test/java/ida/microservices/qcm/QcmApplicationTests.java
@@ -0,0 +1,16 @@
+package ida.microservices.qcm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QcmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
